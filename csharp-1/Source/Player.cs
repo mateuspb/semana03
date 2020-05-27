99983690969d22f5252bdb1631e8af8a65da6178
mateuspb <mateusperin@gmail.com>
2020-05-27 18:25:02 -0300

    Add files via upload

---
+++ csharp-1/Source/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Codenation.Challenge
+{
+    class Player
+    {
+        public Player(long id, long teamId, string name, DateTime birthDate, int skillLevel, decimal salary)
+        {
+            this.Id = id;
+            this.TeamId = teamId;
+            this.Name = name;
+            this.BirthDate = birthDate;
+            this.SkillLevel = skillLevel;
+            this.Salary = salary;
+        }
+        public long Id { get; set; }
+        public long TeamId { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int SkillLevel { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ csharp-1/Source/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Codenation.Challenge
+{
+    class Team
+    {
+        public Team(long id, string name, DateTime createDate, string mainShirtColor, string secondaryShirtColor)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.CreateDate = createDate;
+            this.MainShirtColor = mainShirtColor;
+            this.SecondaryShirtColor = secondaryShirtColor;
+            this.Captain = -1;
+        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string MainShirtColor { get; set; }
+        public string SecondaryShirtColor { get; set; }
+        public long Captain { get; set; }
+    }
+}
